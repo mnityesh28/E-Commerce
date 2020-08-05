f98fa02d2f6e12140146e45190fc5e4444973c1b
NITYESH MAHESHWARI <mnityesh@gmail.com>
2020-08-05 19:46:29 +0530

    Display Price on Home Page

--- shop/views.py
+++ shop/views.py
@@ -42,22 +42,23 @@ def contact(request):
 
 def tracker(request):
     if request.method=="POST":
-        orderId=request.POST.get('orderId','')
-        email=request.POST.get('email','')
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
         try:
-            order=Orders.objects.filter(order_id=orderId,email=email)
+            order = Orders.objects.filter(order_id=orderId, email=email)
             if len(order)>0:
-                update=OrderUpdate.objects.filter(order_id=orderId)
-                updates=[]
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
                 for item in update:
-                    updates.append({'text': item.update_desc, 'time':item.timestamp})
-                    response=json.dumps(updates,default=str)
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps([updates, order[0].items_json], default=str)
                 return HttpResponse(response)
             else:
                 return HttpResponse('{}')
         except Exception as e:
             return HttpResponse('{}')
-    return render(request,'shop/tracker.html')
+
+    return render(request, 'shop/tracker.html')
 def search(request):
     return render(request, 'shop/search.html')
 
